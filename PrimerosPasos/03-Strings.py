ac9ad8aac9844887f5adc82668d80eba23d028ee
CesarSalas <enrique.salas2@educa.madrid.org>
2024-08-01 17:34:28 +0200

    Modificación en los nombre de el Fichero Artimetica -> Operadores y se modifico el orden de los mismos

---
+++ PrimerosPasos/03-Strings.py
@@ -0,0 +1,18 @@
+### Strings ###
+
+my_string = "Mi string"
+my_other_string = 'Mi otro string'
+
+print(len(my_string))
+print(len(my_other_string))
+
+print(my_string + " " + my_other_string)
+
+my_new_line_string = "Este es un String \ncon salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_scape_string = "\tEste es un String \n escapado"
+print(my_scape_string)
